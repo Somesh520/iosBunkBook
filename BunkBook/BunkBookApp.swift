c2678f45d0900be49610c3dff67f370b36d7241c
Someshtiwari532@gmail.com <Shail@123>
2026-02-01 10:49:43 +0530

    Initial Commit

---
+++ BunkBook/BunkBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BunkBookApp.swift
+//  BunkBook
+//
+//  Created by Somesh Tiwari on 01/02/26.
+//
+
+import SwiftUI
+
+@main
+struct BunkBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
